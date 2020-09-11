f95a3dd23f718b49bf0d2aa88dd09c0378b69a87
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-11 14:12:50 +0700

    Fix socket connection

--- socketAPI.js
+++ socketAPI.js
@@ -2,5 +2,10 @@ let socketIO = require('socket.io');
 let io = socketIO();
 let socketAPI = {};
 //Your socket logic here
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+
 socketAPI.io = io;
 module.exports = socketAPI;
