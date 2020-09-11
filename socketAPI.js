de39feed100701c41f3788e0381add7d95bdec79
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-11 14:35:42 +0700

    1- Check user disconnected

--- socketAPI.js
+++ socketAPI.js
@@ -4,8 +4,9 @@ let socketAPI = {};
 //Your socket logic here
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
-
-
 socketAPI.io = io;
 module.exports = socketAPI;
